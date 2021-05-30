b2b6834d8ccd4cd647997bd904615f8e4eee094a
Yoshiaki Togami <tuabobo123@gmail.com>
2021-05-31 01:46:15 +0900

    feat: add error handling when reading files

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-    let filename = &args[1];
-    let mut f = File::open(&filename).expect("There are no files");
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments:{}", err);
+        process::exit(1)
+    });
+    let mut f = File::open(&config.filename).expect("There are no files");
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
     println!("{}", contents);
+}
+
+struct Config {
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+        let filename = args[1].clone();
+        Ok(Config { filename })
+    }
 }
