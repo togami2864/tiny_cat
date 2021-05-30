b920b480d602b6eafbaf9e5fc1b1a2e689ebeef9
Yoshiaki Togami <tuabobo123@gmail.com>
2021-05-31 01:59:20 +0900

    refc: reactor

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub struct Config {
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        }
+        let filename = args[1].clone();
+        Ok(Config { filename })
+    }
+}
+
+pub fn run(filename: &String) -> Result<(), Box<Error>> {
+    let mut f = File::open(&filename)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+    println!("{}", contents);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+extern crate tinycat;
+
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
+use tinycat::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,30 +11,8 @@ fn main() {
         eprintln!("Problem parsing arguments:{}", err);
         process::exit(1)
     });
-    if let Err(e) = run(&config.filename) {
+    if let Err(e) = tinycat::run(&config.filename) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-struct Config {
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
-            return Err("not enough arguments");
-        }
-        let filename = args[1].clone();
-        Ok(Config { filename })
-    }
-}
-
-fn run(filename: &String) -> Result<(), Box<Error>> {
-    let mut f = File::open(&filename)?;
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-    println!("{}", contents);
-    Ok(())
 }
